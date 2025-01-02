850c3b18f889fa4fa110c8a3473e2dadba141168
Oskar Skowroński <97964979+OskarSko@users.noreply.github.com>
2025-01-02 17:30:07 +0100

    Update page.js

--- Lab12/app/page.js
+++ Lab12/app/page.js
@@ -2,6 +2,8 @@ import styles from '../styles/page.module.css';
 import ImageSlideshow from '../components/images/ImageSlideshow';
 import MainHeader from '../components/MainHeader'; // Import komponentu MainHeader
 import Image from 'next/image';
+import Link from 'next/link'; // Import komponentu Link
+
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -10,7 +12,7 @@ export default function Home() {
 
       <header className={styles.header}>
         <div className={styles.logo}>
-        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
+          <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
           <h1>NextLevel Food</h1>
         </div>
       </header>
@@ -26,12 +28,12 @@ export default function Home() {
             <h1>NextLevel Food for NextLevel Foodies</h1>
             <p>Taste & share food from all over the world.</p>
             <div className={styles.cta}>
-              <a href="/community" className={styles.buttonSecondary}>
+              <Link href="/community" className={styles.buttonSecondary}>
                 Join the Community
-              </a>
-              <a href="/meals" className={styles.buttonPrimary}>
+              </Link>
+              <Link href="/meals" className={styles.buttonPrimary}>
                 Explore Meals
-              </a>
+              </Link>
             </div>
           </div>
         </div>
